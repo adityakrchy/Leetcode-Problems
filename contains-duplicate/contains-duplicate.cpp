96305b70a9d8073c43e2f448fef87751c8b18d7a
adityakrchy <34484508+adityakrchy@users.noreply.github.com>
2023-01-10 22:02:40 +0530

    Time: 121 ms, Memory: 53.5 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int i = 0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(auto i:freq){
+            if(i.second > 1){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
